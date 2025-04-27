148a9217a3826389de940e363c5bbde468a45480
scarjo11 <sezzle-wave@live.fr>
2025-04-27 23:24:17 +0200

    config styles.d.ts to recognize images

--- src/styles.d.ts
+++ src/styles.d.ts
@@ -1,7 +1,19 @@
-// For TypeScript to reconize .module.scss
+// For TypeScript to recognize .module.scss
 declare module '*.module.scss' {
     const classes: { [key: string]: string };
     export default classes;
 }
 
+// For TypeScript to recognize .scss
 declare module '*.scss';
+
+// For TypeScript to recognize images
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
